759ed2d0162c49bb73f6fe21c62ed6478a972c07
Alex Walsh <alexwalsh350@gmail.com>
2020-12-24 22:50:51 -0500

    create getworkouts and addworkouts get and post requests

--- functions/index.js
+++ functions/index.js
@@ -17,27 +17,51 @@ admin.initializeApp({credential: admin.credential.cert(require('./admin.json'))}
 const app = express(); //to access express
 const db = admin.firestore(); //simplify access to firestore (the database)
 
+app.post('/addworkout', (req,res) => {
+  //TODO: Make sure user is authenticated
+  db.collection("userworkouts").doc()
+  .set({
+    uid: "8bdIe4HOfoSCEG6j7osKFgcttI02",
+    name: "2 Minuite Workout",
+    moves: ["burpees", "lunges", "pushups"],
+    reps: [3,2,1],
+    sets: [2,3,2]
+  })
+  return res.status(200).json({status: 300});
+  
+});
 
 
-app.get('/addworkout', (req,res) => {
+
+app.get('/getworkouts', (req,res) => {
     cors(req,res, () => {
-        console.log("Adding Workout");
+        // console.log("Adding Workout");
         //if user is signed in
         //add workout to users workout database
+        //TODO: Authenticate User uid = uid 
         //TODO: On signup user workout database should be created with email/username as name
-        db.collection("userworkouts").doc("LCjsWI3T5Q12I5itezEZ")
+        let userworkouts = db.collection("userworkouts");
+        let query = userworkouts.where("uid", "==", req.query.uid);
+        let result;
+        query
         .get()
-        .then((doc) => {
-            let data = doc.data();
-            console.log(data);
-            
-            return res.status(200).json({Movements: data.Workout, Repetitions: data.Repetitions, Sets: data.Sets});
-        }).catch((error) => {
-            console.log("error: ", error);
-            return res.json(error);
-        })
+        .then(function(querySnapshot) {
+          result = [];
+        querySnapshot.forEach(function(doc) {
+            // doc.data() is never undefined for query doc snapshots
+            console.log(doc.id, " => ", doc.data());
+            result.push([doc.id , doc.data()]);
+        });
+        return res.json({data: result});
+    })
+    .catch(function(error) {
+        console.log("Error getting documents: ", error);
     });
-});
+    
+    
+    });
+  });
+
 
 
 app.get('/test', (req,res) => {
@@ -70,6 +94,11 @@ app.get('/signup',(req,res) => {
         friends: []
     })
     console.log('Successfully created new user:', userRecord.uid);
+    db.collection("users").doc(userRecord.uid).set({
+      workouts: ["day 1", "day 2", "day 3"]
+    }).catch((error) => {
+      console.log(error);
+    })
     return res.json({message: "user created"});
   })
   .catch((error) => {
